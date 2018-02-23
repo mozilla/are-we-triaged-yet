5959aa1aab855e4ef6d246b8968aa1ea3483ceeb
emceeaich <ech@emmah.net>
2018-02-22 18:43:44 -0800

    Listen on 8080 unless told otherwise

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get("/data", function (request, response) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 8080, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
